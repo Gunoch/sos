215c9d67319d28c75d8a063b2b0b0d781a898e77
Gustavo Nolasco Chaves <146135610+Gunoch@users.noreply.github.com>
2024-11-03 23:38:36 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+    for (let index = 0; index < 3; index++) {
+    	
+    }
+    for (let index = 0; index < 3; index++) {
+    	
+    }
+})
